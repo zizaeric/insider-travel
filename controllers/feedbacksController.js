d11b638c099f8811d4fe36737d3a1b8e0f179c65
Tyler Kidd <tylerckiddcontact@gmail.com>
2019-06-25 15:37:58 -0400

    controller upload

---
+++ controllers/feedbacksController.js
@@ -0,0 +1,37 @@
+const db = require("../models");
+
+// Defining methods for the feedbacksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Feedback
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Feedback
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Feedback
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Feedback
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Feedback
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
